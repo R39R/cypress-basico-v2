b5b3af23f0986ecd6c7454f4cf2b186137b2762b
Vinícius Rodrigues da Rosa <vrosa@narwalsistemas.com.br>
2024-05-02 10:06:28 -0300

    aaaa

---
+++ cypress/integration/narwal.spec.js
@@ -0,0 +1,35 @@
+/// <reference types="cypress"/>
+
+describe('Portal Narwal', () => {
+    beforeEach(() => {
+        cy.visit('cliente1regression.narwalsistemas.com.br')
+    })
+
+    it('Logar e criar admin', () => {
+        cy.get('#Login')
+            .type('admin', {delay: 0})
+        cy.get('#senha')
+            .type('S3gur@nc4-N@rv4lS1stem@s', {delay: 0})
+        cy.get('.send > input')
+            .click({force: true})
+        cy.get('[data-id="1"] > .k-header')
+            .click({force: true})
+        cy.get('[data-id="19"] > .k-link')
+            .click({force: true})
+        cy.get('#btnNovo')
+            .click({waitForAnimations: true})
+        cy.get(':nth-child(3) > ul > .tbValue > #Nome')
+            .type('TesteCy3', {delay: 0})
+        cy.get(':nth-child(5) > ul > .tbValue > #Email')
+            .type('TesteCy3@narwalsistemas.com.br', {delay: 0})
+        cy.get(':nth-child(4) > ul > .tbValue > #Sobrenome')
+            .type('TesteCy3', {delay: 0})
+        cy.get(':nth-child(11) > .tbItem2 > ul > .tbValue > #Login')
+            .type('TesteCy3', {delay: 0})
+        cy.get('.tbValue > .k-widget > .k-dropdown-wrap > .k-input')
+            .type('{downArrow}')
+            .type('{enter}')
+        cy.get('#btnAdicionar')
+            .click()
+    })
+})
